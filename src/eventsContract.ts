8243c6642627aea29273bf54cd4a244e8fac4673
NickWhiteman <66336085+NickWhiteman@users.noreply.github.com>
2023-03-18 01:35:37 +0300

    Update eventsContract.ts
    fix

--- src/eventsContract.ts
+++ src/eventsContract.ts
@@ -7,7 +7,7 @@ import { eventName } from './utils/contractEventKeys';
  * @description The array must contain keys, similar to TypeContract
  * @file See ./types.d.ts
  */
-const contractType = ['token', 'sale', 'staking', 'freeze', 'level', 'repo', 'option'];
+const contractType: string[] = [];
 
 /**
  * @description This function must be called the first time the application is rendered on the client to handle events.
